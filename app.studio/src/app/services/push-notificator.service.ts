5c231e4f9133b5aa47fc3b18aa7f7d1601f2717b
Felipe Pires <felipeaugustodluz@hotmail.com>
2023-07-23 05:35:42 -0300

    Ajustando notificator.

--- app.studio/src/app/services/push-notificator.service.ts
+++ app.studio/src/app/services/push-notificator.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
 import { Observable } from 'rxjs';
 import { ApiService } from './api-service.service';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +13,24 @@ export class PushNotificatorService {
   readonly VAPID_PUBLIC_KEY = "BNm2RvdTrp3OMyU-MrrwCRKi8LPpjLQqSrxEHG20lceWndqPDnDOI4rxu9OrQJOu62A4mueB93God48gu2hjzqA";
 
   constructor(
+      private localStorage: LocalStorageService,
       private apiService: ApiService,
       private swPush: SwPush) {}
 
   subscribeToNotifications() {
-      !this.swPush.isEnabled &&
+      !this.localStorage.get("notificator") &&
       this.swPush.requestSubscription({
           serverPublicKey: this.VAPID_PUBLIC_KEY
       })
-      .then(sub => this.apiService.addSubscriber(sub).subscribe(x => console.log(x)))
-      .catch(err => console.error("Could not subscribe to notifications ", err));
+      .then(sub => {
+        this.apiService.addSubscriber(sub).subscribe(x => console.log(x));
+        this.localStorage.set("notificator", true);
+        
+      })
+      .catch(err => {
+        this.localStorage.set("notificator", false);
+        console.error("Could not subscribe to notifications ", err)
+      });
 
       this.swPush.messages.forEach(x => {
         console.log(x);
