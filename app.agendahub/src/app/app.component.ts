fbb9bbc868bcf8643d060b698162e4c09cfbab43
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-02-04 20:46:16 -0300

    update login style

--- app.agendahub/src/app/app.component.ts
+++ app.agendahub/src/app/app.component.ts
@@ -1,28 +1,34 @@
-import { Component } from '@angular/core';
-import * as moment from 'moment';
-import { PrimeNGConfig } from 'primeng/api';
-import { Router } from '@angular/router';
-import { skipRoutes } from './models/core/rules';
-import { Title } from '@angular/platform-browser';
+import { Component } from "@angular/core";
+import * as moment from "moment";
+import { PrimeNGConfig } from "primeng/api";
+import { Router } from "@angular/router";
+import { skipRoutes } from "./models/core/rules";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
     <loader></loader>
-    <p-toast [breakpoints]="{'920px': {width: '100%', right: '0', left: '0'}}"></p-toast>
+    <p-toast
+      [breakpoints]="{ '920px': { width: '100%', right: '0', left: '0' } }"
+    ></p-toast>
     <!-- <app-nav *ngIf="!isLogin && !hideNav"></app-nav> -->
 
-    <div class="w-screen h-screen overflow-auto dark:bg-primary backdrop-blur-lg bg-clean" [ngClass]="{'flex': sidebarFixed}">
-
+    <div
+      class="w-screen h-screen overflow-auto dark:bg-primary backdrop-blur-lg bg-clean"
+      [ngClass]="{ flex: sidebarFixed }"
+    >
       <sidebar *ngIf="!isLogin && !hideNav"></sidebar>
 
-      <div class="relative w-full overflow-auto" [ngClass]="{'sm:pl-14 pl-0 ': !isLogin && !hideNav}">
+      <div
+        class="relative w-full overflow-auto"
+        [ngClass]="{ 'sm:pl-14 pl-0 ': !isLogin && !hideNav }"
+      >
         <router-outlet></router-outlet>
       </div>
-
     </div>
 
-  <!-- <div class="isolate overflow-hidden">
+    <!-- <div class="isolate overflow-hidden">
     <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] h-full overflow-h-scroll" aria-hidden="true">
       <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
       <div class="md:hidden block relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
@@ -34,36 +40,35 @@ import { Title } from '@angular/platform-browser';
       <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
   </div> -->
-
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-
-  constructor(private primeNG: PrimeNGConfig, private router: Router, private title: Title) {
+  constructor(
+    private primeNG: PrimeNGConfig,
+    private router: Router,
+    private title: Title
+  ) {
     // moment.updateLocale("pt-br", null)
 
-    moment.locale("pt-br")
-  
-    
+    moment.locale("pt-br");
+
     primeNG.overlayOptions = {
-      appendTo: "body"
-    }
+      appendTo: "body",
+    };
 
-    title.setTitle("AgendaHub | Sistema de Gestão de Atividades")
-
+    title.setTitle("AgendaHub | Sistema de Gestão de Atividades");
   }
 
   get hideNav() {
-    return skipRoutes.includes(location.pathname.replaceAll("/", ""))
+    return skipRoutes.includes(location.pathname.replaceAll("/", ""));
   }
 
   get isLogin() {
-    return this.router.url.includes("login")
+    return this.router.url.includes("login");
   }
 
   get sidebarFixed() {
-    return localStorage.getItem("sidebarFixed") === "true"
+    return localStorage.getItem("sidebarFixed") === "true";
   }
-
 }
