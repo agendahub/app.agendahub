7f2fdf74e7f308f9cc7d05acc536d76f5c3289e7
nsx07 <felipeaugustodluz@hotmail.com>
2024-03-10 00:49:28 -0300

    adicionando ErrorHandler

--- app.agendahub/src/app/services/api-service.service.ts
+++ app.agendahub/src/app/services/api-service.service.ts
@@ -8,6 +8,7 @@ import { LocalStorageService } from './local-storage.service';
 import { MessageService } from 'primeng/api';
 import { AuthService } from '../auth/auth-service.service';
 import { ErrorDto } from '../models/dtos/dtos';
+import { ErrorHandler } from '../utils/error';
 
 export type QueryParams = Record<string, string | number | boolean | ReadonlyArray<string | number | boolean > | Array<number|string>>;
 
@@ -50,7 +51,7 @@ export class ApiService {
       load && this.loader.hide();
       return result;
     }), catchError(err => {
-      this.templateError(err);
+      ErrorHandler.templateError(err, this.messageService, this.auth);
       throw err;
     }));
   }
@@ -63,7 +64,7 @@ export class ApiService {
       load && this.loader.hide();
       return result;
     }), catchError(err => {
-      this.templateError(err);
+      ErrorHandler.templateError(err, this.messageService, this.auth);
       throw err;
     }));
   }
@@ -76,7 +77,7 @@ export class ApiService {
       load && this.loader.hide();
       return result;
     }), catchError(err => {
-      this.templateError(err);
+      ErrorHandler.templateError(err, this.messageService, this.auth);
       throw err;
     }));
   }
@@ -89,7 +90,7 @@ export class ApiService {
       load && this.loader.hide();
       return result;
     }), catchError(err => {
-      this.templateError(err);
+      ErrorHandler.templateError(err, this.messageService, this.auth);
       throw err;
     }));
   }
@@ -102,42 +103,15 @@ export class ApiService {
       subscriptionObject: JSON.stringify(sub),
       user : this.localStorage.get("user")
     }
-
-    // console.log(subscription);
     
     return this.httpClient.post(this.baseUrl + "User/Subscribe", subscription).pipe(take(1)).pipe(map(result => {
       this.loader.hide();
       return result;
     }), catchError(err => {
-      this.templateError(err);
-      throw err;
+      this.loader.hide();
+      ErrorHandler.templateError(err, this.messageService, this.auth);
+      return err;
     }));
   }
 
-  private templateError(error: any) {
-    
-    this.loader.hide();
-    console.log(error);    
-
-    if (error.status) {
-
-      if (error.error?.message) {
-        this.messageService.add({severity: "error", summary: "Erro ao salvar!", detail: error.message});
-        return void 0;
-      }
-
-      if (error.status >= 400 && error.status < 500) {
-
-        if (error.status === 401) {
-          this.auth.logout(() => this.messageService.add({severity: "error", summary: "Erro ao validar identidade!", detail: "É necessário realizar o login."}));
-          return void 0;
-        }
-
-      } else {
-        this.messageService.add({severity: "error", summary: "Erro desconhecido!", detail: "O servidor falhou catastroficamente"})
-      }
-
-    }
-  
-  }
 }
