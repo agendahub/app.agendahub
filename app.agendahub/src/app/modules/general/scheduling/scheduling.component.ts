749cb3986fd6779ea776daa4d6ef41b2ed3febf1
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2024-03-14 21:29:37 -0300

    [add] scheduling list

--- app.agendahub/src/app/modules/general/scheduling/scheduling.component.ts
+++ app.agendahub/src/app/modules/general/scheduling/scheduling.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../services/api-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { GetTableSchedulingListDto } from '../../../models/dtos/dtos';
+
+
 
 @Component({
   selector: 'app-scheduling',
@@ -6,6 +12,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./scheduling.component.scss']
 })
 export class SchedulingComponent {
-
   rangeDates: Date[] | undefined;
+  
+  searchClient: string = '';
+  schedulingList: GetTableSchedulingListDto[] = [];
+
+  constructor(private apiService: ApiService, private messageService: MessageService) { }
+
+  getSchedulingList() {
+    if (!this.rangeDates || this.rangeDates.length !== 2) {
+      return;
+    }
+
+    const startDate = this.rangeDates[0].toISOString(); 
+    const endDate = this.rangeDates[1].toISOString();
+
+    this.apiService.requestFromApi<any>('Schedule/GetTableSchedulingList', { startDate, endDate })
+      .subscribe({
+        next: (response: any) => {
+          if (response && response.length > 0) {
+            this.schedulingList = response.map((item: any) => ({
+              name: item.customerName,
+              startDate: item.startDate,
+              finishDate: item.endDate,
+              appointmentCount: item.total
+            }));
+          } else {
+            this.schedulingList = [];
+
+          }
+        },
+        error: (error: any) => {
+          console.error('Error fetching scheduling list:', error);
+        }
+      });
+  }
+  
+
 }
