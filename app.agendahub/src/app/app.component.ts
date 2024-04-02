efa4f5b3b6202ad4c19b5b99892f3bf0ca1f0741
nsx07 <felipeaugustodluz@hotmail.com>
2024-04-01 23:59:12 -0300

    ajustando nav e sidebar and home

--- app.agendahub/src/app/app.component.ts
+++ app.agendahub/src/app/app.component.ts
@@ -12,34 +12,21 @@ import { Title } from "@angular/platform-browser";
     <p-toast
       [breakpoints]="{ '920px': { width: '100%', right: '0', left: '0' } }"
     ></p-toast>
-    <!-- <app-nav *ngIf="!isLogin && !hideNav"></app-nav> -->
 
     <div id="app-container"
       class="w-screen h-screen overflow-auto dark:bg-primary backdrop-blur-lg bg-clean"
       [ngClass]="{ flex: sidebarFixed }"
     >
+      
       <sidebar *ngIf="!isLogin && !hideNav"></sidebar>
 
-      <div
-        class="relative w-full md:h-full h-max overflow-auto"
-        [ngClass]="{ 'sm:pl-14 pl-0 ': !isLogin && !hideNav }"
-      >
+      <div class="relative w-full md:h-full h-max overflow-auto" [ngClass]="{ 'sm:pl-2 pl-0 ': !isLogin && !hideNav }">
+        <div class="sm:block hidden" [ngClass]="{'-ml-1': sidebarFixed, 'pl-2': !sidebarFixed}">
+          <app-nav></app-nav>
+        </div>
         <router-outlet></router-outlet>
       </div>
     </div>
-
-    <!-- <div class="isolate overflow-hidden">
-    <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] h-full overflow-h-scroll" aria-hidden="true">
-      <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-      <div class="md:hidden block relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>  
-    
-
-    <div class="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:bottom-10 " aria-hidden="true">
-      <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>
-  </div> -->
   `,
   styles: [],
 })
--- app.agendahub/src/app/components/sidebar/sidebar.component.ts
+++ app.agendahub/src/app/components/sidebar/sidebar.component.ts
@@ -79,7 +79,7 @@ import { MenuItem } from "primeng/api";
     </div>
 
     <aside
-      class="inset-0 sm:inset-1 sm:z-20 z-[60] w-screen sm:h-[99vh] h-screen ease-in-out duration-300"
+      class="sm:z-20 z-[60] w-screen h-screen ease-in-out duration-300"
       aria-label="Sidebar"
       id="default-sidebar"
       [ngClass]="{
@@ -92,7 +92,7 @@ import { MenuItem } from "primeng/api";
       (dblclick)="setFixed()"
     >
       <div
-        class="h-full overflow-y-auto scroll- overflow-x-hidden bg-very-clean dark:bg-secondary flex flex-col justify-between gap-3 rounded-none sm:rounded-xl pb-1"
+        class="h-full overflow-y-auto scroll- overflow-x-hidden bg-very-clean dark:bg-primary border-l-0 border-r-2 dark:border-secondary border-very-clean flex flex-col justify-between gap-3 pb-1"
         [ngClass]="{ 'px-2': open }"
       >
         <div class="flex flex-col gap-3">
@@ -203,7 +203,10 @@ import { MenuItem } from "primeng/api";
                 class="cursor-pointer flex items-center p-4 text-primary rounded-lg dark:text-white hover:bg-primary hover:text-white dark:hover:bg-white dark:hover:text-primary group"
                 [ngClass]="{ 'justify-center': !open }"
               >
-                <i class="fa-solid fa-calendar-days w-5"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                </svg>
+
                 <span class="ms-3 select-none" *ngIf="open">Atendimentos</span>
               </a>
             </li>
