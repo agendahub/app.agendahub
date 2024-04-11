50c651269535816718c4b98b2108ee2217a3527a
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-04-10 21:47:52 -0300

    feat: add upcoming events

--- app.agendahub/src/app/pages/home/home.component.ts
+++ app.agendahub/src/app/pages/home/home.component.ts
@@ -1,48 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { LoaderService } from '../../services/loader.service';
-import { EventService } from '../../models/services/event.service';
-import { User, UserSchedule } from '../../models/core/entities';
-import { SchedulesDateRangeEnum } from '../../models/core/enums';
-import { FormControl } from '@angular/forms';
-import { AuthService } from '../../auth/auth-service.service';
-import { faCalendar } from '@fortawesome/free-solid-svg-icons';
-import { OverlayPanel } from 'primeng/overlaypanel';
-import { mapScheduleToEvent } from '../../utils/util';
-import { EventInput } from '@fullcalendar/core';
-import { ScreenHelperService } from '../../services/screen-helper.service';
-import * as moment from 'moment';
+import { Component, OnInit } from "@angular/core";
+import { LoaderService } from "../../services/loader.service";
+import { EventService } from "../../models/services/event.service";
+import { User, UserSchedule } from "../../models/core/entities";
+import { SchedulesDateRangeEnum } from "../../models/core/enums";
+import { FormControl } from "@angular/forms";
+import { AuthService } from "../../auth/auth-service.service";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { OverlayPanel } from "primeng/overlaypanel";
+import { mapScheduleToEvent } from "../../utils/util";
+import { EventInput } from "@fullcalendar/core";
+import { ScreenHelperService } from "../../services/screen-helper.service";
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-
   calendarOpen = false;
   faCalendar = faCalendar;
-  
+
   events: UserSchedule[] = [];
-  eventsCalendar: EventInput[] = []
-  dateRanges = [{label:"Dia", index: 0}, {label:"Semana", index: 1}, {label:"Mês", index: 2}];
-  dateRange: FormControl = new FormControl<{label: string, index: SchedulesDateRangeEnum}>(this.dateRanges[1]);
+  eventsCalendar: EventInput[] = [];
+  dateRanges = [
+    { label: "Dia", index: 0 },
+    { label: "Semana", index: 1 },
+    { label: "Mês", index: 2 },
+  ];
+  dateRange: FormControl = new FormControl<{
+    label: string;
+    index: SchedulesDateRangeEnum;
+  }>(this.dateRanges[2]);
 
   user!: User;
 
-  constructor(private eventService: EventService, private auth: AuthService, public scHelp: ScreenHelperService) {
+  constructor(
+    private eventService: EventService,
+    private auth: AuthService,
+    public scHelp: ScreenHelperService
+  ) {
     this.user = auth.getUserData();
   }
-  
+
   ngOnInit(): void {
     this.getEvents();
   }
 
   private getEvents() {
-    this.eventService.getCurrentEvents(this.dateRange.value.index)?.subscribe(x => {
-      this.events = x;
-      console.log(x);
-      
-    });
+    this.eventService
+      .getCurrentEvents(this.dateRange.value.index)
+      ?.subscribe((x) => {
+        this.events = x.slice(0, 3);
+        console.log(x);
+      });
   }
 
   getDayName(date: Date) {
@@ -50,15 +61,18 @@ export class HomeComponent implements OnInit {
   }
 
   getWidth() {
-    switch(this.scHelp.currentDevice()) {
-      case 2: return "85%"
-      case 1: return "65%"
-      default: return "50%"
+    switch (this.scHelp.currentDevice()) {
+      case 2:
+        return "85%";
+      case 1:
+        return "65%";
+      default:
+        return "50%";
     }
   }
 
   getColor(userSchedule: UserSchedule) {
-    return userSchedule.employee.color ?? "#1da1f2"
+    return userSchedule.employee.color ?? "#1da1f2";
   }
 
   onChangeDateRange(event: any) {
@@ -68,13 +82,13 @@ export class HomeComponent implements OnInit {
   showCalendar(event: any, op: OverlayPanel, schedule: UserSchedule) {
     op.toggle(event);
     this.eventsCalendar = [];
-    
+
     if (this.calendarOpen) {
       this.calendarOpen = false;
-    } else setTimeout(() => {
-            this.calendarOpen = true;
-            this.eventsCalendar = mapScheduleToEvent([schedule]);
-    });
+    } else
+      setTimeout(() => {
+        this.calendarOpen = true;
+        this.eventsCalendar = mapScheduleToEvent([schedule]);
+      });
   }
-
 }
