016cee73d6a8aff11911f626a058fed067b4be2c
nsx07 <felipeaugustodluz@hotmail.com>
2024-05-23 23:45:43 -0300

    avanços relatorio dash

--- app.agendahub/src/app/modules/manager/reports/reports.module.ts
+++ app.agendahub/src/app/modules/manager/reports/reports.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { TabMenuModule } from "primeng/tabmenu";
 import { SharedModule } from "../../../shared/shared.module";
 import { ReportsRoutingModule } from "./reports-routing.module";
 import { ReportsComponent } from "./reports/reports.component";
 
 @NgModule({
   declarations: [ReportsComponent],
-  imports: [SharedModule, ReportsRoutingModule],
+  imports: [SharedModule, ReportsRoutingModule, TabMenuModule, NgxChartsModule],
 })
 export class ReportsModule {}
--- app.agendahub/src/app/modules/manager/reports/reports/reports.component.ts
+++ app.agendahub/src/app/modules/manager/reports/reports/reports.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MenuItem, PrimeNGConfig } from "primeng/api";
+import { firstValueFrom } from "rxjs";
+import { ApiService } from "../../../../services/api-service.service";
 
 @Component({
-  selector: 'app-reports',
-  templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  selector: "app-reports",
+  templateUrl: "./reports.component.html",
+  styleUrls: ["./reports.component.scss"],
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit {
+  item!: MenuItem;
 
+  items: MenuItem[] = [
+    { label: "Resumo", icon: "pi pi-fw pi-home" },
+    // { label: "Serviços", icon: "pi pi-fw pi-tags" },
+    // { label: "Clientes", icon: "pi pi-fw pi-users" },
+    // { label: "Funcionários", icon: "pi pi-fw pi-dollar" },
+  ];
+
+  preview: any = null;
+  chartData = [] as any[];
+
+  constructor(private prime: PrimeNGConfig, private api: ApiService) {
+    prime.ripple = false;
+  }
+
+  ngOnInit(): void {
+    this.item = this.items[0];
+
+    firstValueFrom(this.api.requestFromApi("api/UserSchedule/GetWeekPreviewReport")).then((data) => {
+      this.preview = data;
+      this.chartData = data.series;
+    });
+  }
 }
