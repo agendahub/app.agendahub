5f9c17a9b57f22abc420ccc44310591d336a24bd
Felipe Pires <97983021+nsx07@users.noreply.github.com>
2024-05-24 21:54:42 -0300

    Atualizar o environment.ts

--- app.agendahub/src/environments/environment.ts
+++ app.agendahub/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   uiUrl: window.location.host + "/",
-  apiUrl: "https://apiagendahub-web-push.up.railway.app/",
+  apiUrl: "https://apiagendahub-demo.up.railway.app/",
   getApiDomain: () => new URL(environment.apiUrl).hostname,
 };
