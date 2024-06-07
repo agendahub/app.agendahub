5cf975e9050875a2f51e1ee6a0c42559556dda11
nsx07 <felipeaugustodluz@hotmail.com>
2024-06-07 12:58:55 -0300

    adicionando checkbox ativo e ajustes

--- app.agendahub/src/app/modules/manager/manager.module.ts
+++ app.agendahub/src/app/modules/manager/manager.module.ts
@@ -1,40 +1,35 @@
-import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 
-import { ManagerRoutingModule } from './manager-routing.module';
-import { ServicesComponent } from './services/services.component';
-import { UsersComponent } from './users/users.component';
-import { ManagerComponent } from './manager.component';
+import { ManagerRoutingModule } from "./manager-routing.module";
+import { ManagerComponent } from "./manager.component";
+import { ServicesComponent } from "./services/services.component";
+import { UsersComponent } from "./users/users.component";
 
-import { DataViewModule, DataViewLayoutOptions } from 'primeng/dataview';
-import { TagModule } from "primeng/tag"
-import { TableModule } from "primeng/table"
-import { DialogModule } from "primeng/dialog"
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { ColorPickerModule } from 'primeng/colorpicker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputNumberModule } from 'primeng/inputnumber';
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ButtonModule } from "primeng/button";
+import { CalendarModule } from "primeng/calendar";
+import { CheckboxModule } from "primeng/checkbox";
+import { ColorPickerModule } from "primeng/colorpicker";
+import { DataViewModule } from "primeng/dataview";
+import { DialogModule } from "primeng/dialog";
+import { DropdownModule } from "primeng/dropdown";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextModule } from "primeng/inputtext";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { OverlayPanelModule } from "primeng/overlaypanel";
+import { TableModule } from "primeng/table";
+import { TagModule } from "primeng/tag";
 
 @NgModule({
-  declarations: [
-    ServicesComponent,
-    UsersComponent,
-    ManagerComponent
-  ],
+  declarations: [ServicesComponent, UsersComponent, ManagerComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     ManagerRoutingModule,
     FontAwesomeModule,
-
 
     ButtonModule,
     DropdownModule,
@@ -47,7 +42,8 @@ import { InputNumberModule } from 'primeng/inputnumber';
     DataViewModule,
     DialogModule,
     TableModule,
-    TagModule
-  ]
+    TagModule,
+    CheckboxModule,
+  ],
 })
-export class ManagerModule { }
+export class ManagerModule {}
